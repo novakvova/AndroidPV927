d3431e5f664f2bde78c2e9fe81ee518903a97c0d
novakvova <novakvova@gmail.com>
2021-09-28 19:32:00 +0300

    base app android

--- Android/app/src/main/java/com/example/shopauto/MainActivity.java
+++ Android/app/src/main/java/com/example/shopauto/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.shopauto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtInfo = findViewById(R.id.txtInfo);
+    }
+    public void onClickInfo(View view) {
+        Toast.makeText(this,txtInfo.getText(), Toast.LENGTH_LONG).show();
     }
 }
